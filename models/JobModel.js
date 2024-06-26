a1e3d8a790b152e2ebd48ad570fd4967595f2d51
BBJ <yadhumv365@gmail.com>
2024-06-26 12:27:45 +0530

    initial

--- models/JobModel.js
+++ models/JobModel.js
@@ -4,65 +4,30 @@ const newJobsSchema = new mongoose.Schema({
     title:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     description:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     companyName:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     experience:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     qualification:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     salary:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
     location:{
         type:String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        serverSelectionTimeoutMS: 30000,  
-        socketTimeoutMS: 30000  
     },
 })
 module.exports = mongoose.model("Jobs",newJobsSchema)
--- models/userModel.js
+++ models/userModel.js
@@ -5,9 +5,7 @@ const userSchema = new mongoose.Schema({
         type :String,
         required:true,
         minLength:3,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        
         serverSelectionTimeoutMS: 30000,  
         socketTimeoutMS: 30000 ,
         trim: true,  
@@ -16,9 +14,7 @@ const userSchema = new mongoose.Schema({
         type :String,
         required:true,
         maxLength:50,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        
         serverSelectionTimeoutMS: 30000,  
         socketTimeoutMS: 30000  ,
         trim: true,  
@@ -30,9 +26,7 @@ const userSchema = new mongoose.Schema({
         required:true,
         minLength:3,
         maxLength:20,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        
         serverSelectionTimeoutMS: 30000,  
         socketTimeoutMS: 30000 ,
         trim: true,   
@@ -40,9 +34,7 @@ const userSchema = new mongoose.Schema({
     password:{
         type :String,
         require:true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        
         serverSelectionTimeoutMS: 30000,  
         socketTimeoutMS: 30000 ,
         
