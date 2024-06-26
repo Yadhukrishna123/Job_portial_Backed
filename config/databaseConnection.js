cc798303b71d1932aab89960403826e9c1699d6f
BBJ <yadhumv365@gmail.com>
2024-06-26 12:16:32 +0530

    initial

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 
  const databaseConnection = ()=>{
-     mongoose.connect(process.env.DB_URI)
+     mongoose.connect(process.env.DB_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,    
+        useFindAndModify: false
+     })
     .then((data)=>console.log(`Databade connected with ${data.connection.host}`))
     .catch((err)=>console.log(err.message))
 }
